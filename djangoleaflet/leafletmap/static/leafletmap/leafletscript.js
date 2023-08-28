57d4b73d219723c6caa38e669bc7ff746b4e9d69
taaviok <taaviok3@gmail.com>
2023-08-28 11:23:00 +0300

    Added geocoding and ability to mark two cities on the map

--- djangoleaflet/leafletmap/static/leafletmap/leafletscript.js
+++ djangoleaflet/leafletmap/static/leafletmap/leafletscript.js
@@ -1,4 +1,39 @@
+var lmap;
+var marker1 = L.marker([0.0, 0.0]);
+var marker2 = L.marker([0.0, 0.0]);
+
 function map_init_basic(map, options) {
-  L.marker([60.1107, 25.0187]).addTo(map);
-  L.marker([15.8573, 74.5069]).addTo(map);
+  lmap = map;
 }
+
+function markCity() {
+  var city1 = document.getElementById('icity1').value;
+  var city2 = document.getElementById('icity2').value;
+
+  const city1URL = 'https://nominatim.openstreetmap.org/search?q=' + city1 + '&format=geojson';
+
+  fetch(city1URL)
+    .then(response => response.json())
+    .then(data => {
+      
+      let coordinates = data.features[0].geometry.coordinates;
+      coordiantes = coordinates.reverse();
+      marker1.removeFrom(lmap)
+      marker1 = L.marker(coordinates);
+      marker1.addTo(lmap);
+    })
+
+  const city2URL = 'https://nominatim.openstreetmap.org/search?q=' + city2 + '&format=geojson';
+
+  fetch(city2URL)
+    .then(response => response.json())
+    .then(data => {
+
+      let coordinates = data.features[0].geometry.coordinates;
+      coordiantes1 = coordinates.reverse();
+      marker2.removeFrom(lmap)
+      marker2 = L.marker(coordinates);
+      marker2.addTo(lmap);
+    })
+}
+
